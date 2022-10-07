0df112ff2459d8247be5558768b2e98f2916c500
Jan Dub <dubstolcova@gmail.com>
2022-10-07 17:43:47 +0200

    Create Password_Generator.py

---
+++ Mini_projects/Password_Generator.py
@@ -0,0 +1,19 @@
+import random
+
+
+print("Welcome stranger to my Password Generator")
+
+characters = "0123456789yxcvbnmlkjhgfdsaqwertzuiopYXCVBNMLKJHGFDSAQWERTZUIOP"
+password_lenght= input("Your passwords lenght")
+password_lenght= int(password_lenght)
+number_of_passwords= input("How many passwords do we want ?")
+number_of_passwords=int(number_of_passwords)
+
+print("Here we go")
+
+for o in range(number_of_passwords):
+    passwords = ""
+    for k in range(password_lenght):
+        passwords += random.choice(characters)
+    print(passwords)
+
