a0edb40eeaa27a5c412ba6350cd9b462f717aeed
Jan Dub <dubstolcova@gmail.com>
2022-10-15 19:09:41 +0200

    added more symbols for better coverage

--- Mini_projects/Password_Generator.py
+++ Mini_projects/Password_Generator.py
@@ -3,7 +3,7 @@ import random
 
 print("Welcome stranger to my Password Generator")
 
-characters = "0123456789yxcvbnmlkjhgfdsaqwertzuiopYXCVBNMLKJHGFDSAQWERTZUIOP"
+characters = "0123456789yxcvbnmlkjhgfdsaqwertzuiopYXCVBNMLKJHGFDSAQWERTZUIOP?:_'/(%!=+*#&$@"	
 password_lenght= input("Your passwords lenght")
 password_lenght= int(password_lenght)
 number_of_passwords= input("How many passwords do we want ?")
