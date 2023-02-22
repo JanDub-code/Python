37c2ae895ab8d828f453a373f353ca126ae03a5a
Dub <dubstolcova@gmail.com>
2023-02-22 17:19:43 +0100

    class number 02 of python

---
+++ School_classes/Structures.py
@@ -0,0 +1,14 @@
+elephant = [1, 2, 3, 4, 5, "ultima","segmentum"]
+print(elephant)
+elephant.pop()
+print(elephant)
+print(type(elephant))
+print(elephant[5][3])
+print(elephant[5][3:7])
+
+
+
+nicolas_cage = ({'name': 'Nicolas', 'surname': 'Cage', 'age': 55})
+
+print(nicolas_cage['name'])
+print(type(nicolas_cage))
