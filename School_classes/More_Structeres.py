46a4599f29cda4d4cc2f6277729aba80e76a2a59
Dub <dubstolcova@gmail.com>
2023-02-23 18:13:38 +0100

    end of class  exercise 2

--- School_classes/More_Structeres.py
+++ School_classes/More_Structeres.py
@@ -25,3 +25,47 @@ print(list3)
 
 cislo = list(range(0,101,5))
 print(cislo)
+
+list_of_lists = [["Herman",50],["susan", 60],["joseph", 70]]
+print(list_of_lists[-1][0])
+
+set1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+set2= {1, 2, 3, 4, 5, 44,55,77,88,990}
+
+set4 = set1.union(set2)
+print(set4)
+
+mzk= {'Harry Potter','Dvacet tisic mil pod morem','Hoši z Marsu','Klub věrných'}
+mahenova = {'Auf der Suche nach der verlorenen Zeit','Klub věrných','Kniha o mrtvých'}
+
+print(mzk.intersection(mahenova))
+
+print(mahenova.difference(mzk))
+
+vstup = input("pokud souhlasis napis : ano ")
+if vstup in ["ano","okay"]:
+    print("diky")
+else:
+    print("chcípni hajzle")
+
+    
+    n=0
+while(n !=100):
+    print("už to nikdy neudělám")
+    n+=1
+
+for i in range(0,100):
+    print("už to nikdy neudělám")
+
+
+liststudentu = []
+for i in range(0,5):
+    liststudentu.append(input("zadej jmeno studenta : "))
+
+liststudentu.sort()
+print(liststudentu)
+
+for i in range(0,5):
+    print("*"*i)
+for i in range(5,0,-1):
+    print("*"*i)
