f1831e342146d9e8dba37e5cfc8b107094b6835c
Jan Dub <dubstolcova@gmail.com>
2023-03-12 22:04:02 +0100

    updated version for progress

--- Math/pdf.py
+++ Math/pdf.py
@@ -5,18 +5,17 @@ from reportlab.pdfgen import canvas
 from PyPDF2 import PdfFileMerger, PdfFileReader
 import os
 
-# Define the headlines and text you want to include in the PDF
-headlines = ["Headline 1", "Headline 2", "Headline 3"]
-text = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
-        "Vivamus consectetur neque vel lectus tristique malesuada.", 
-        "Duis vitae elit vel sapien pretium tincidunt."]
+# Define the headlines, text, and image you want to include in the PDF
+headlines = ["Python projekt"]
+text = ["""hehe"""]
+image_path = "graf.png"
 
 # Set up the ReportLab canvas and stylesheet
-canvas = canvas.Canvas("output.pdf", pagesize=letter)
+canvas = canvas.Canvas("Python_projekt.pdf", pagesize=letter)
 styles = getSampleStyleSheet()
 style = styles['Normal']
 
-# Define the x and y coordinates for the headlines and text
+# Define the x and y coordinates for the headlines, text, and image
 x_coord = 1.5 * inch
 y_coord = 10 * inch
 
@@ -28,16 +27,19 @@ for headline, text in zip(headlines, text):
     canvas.drawString(x_coord, y_coord - 0.5 * inch, text)
     y_coord -= 1 * inch
 
+# Add the image to the canvas
+canvas.drawImage(image_path, x_coord, y_coord - 3 * inch, width=3*inch, height=3*inch)
+
 # Save the canvas to a PDF file
 canvas.save()
 
 # Merge the generated PDF with an existing PDF (optional)
 existing_pdf = PdfFileReader(open("existing.pdf", "rb"))
-output_pdf = PdfFileReader(open("output.pdf", "rb"))
+output_pdf = PdfFileReader(open("Python_projekt.pdf", "rb"))
 merger = PdfFileMerger()
 merger.append(existing_pdf)
 merger.append(output_pdf)
 merger.write("merged.pdf")
 
 # Remove the temporary output file
-os.remove("output.pdf")
+os.remove("Python_projekt.pdf")
