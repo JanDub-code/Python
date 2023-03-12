e47c26221346845d17cc3ee9be041169af14e1ca
Jan Dub <dubstolcova@gmail.com>
2023-03-12 22:02:42 +0100

    testing

---
+++ Math/Graph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+
+# Vytvoření grafu pomocí Matplotlib
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+
+# Uložení grafu do souboru
+plt.savefig('graf.png')
+
