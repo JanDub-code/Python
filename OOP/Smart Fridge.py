2cbdc450c14127caba0d3c9b6afee8754713ee87
Dub <dubstolcova@gmail.com>
2023-03-15 17:37:20 +0100

    now it behaves as it should

--- OOP/Smart Fridge.py
+++ OOP/Smart Fridge.py
@@ -9,6 +9,7 @@ class SmartFridge:
         self.model = model
         self.birth_date = date.today()                  
         SmartFridge.fridge_count += 1
+        self.food = []
         
     def greet(self):
         print(f'Hello from {self.brand} {self.model}!')
@@ -18,12 +19,15 @@ class SmartFridge:
         print('I am {} days old.'.format(td.days))
 
     def add_food(self, food):
-        self.food = food
-        print(f'I have {self.food} in my fridge.')
+        self.food.append(food)
+        print(f'I have added {food} in my fridge.')
 
     def remove_food(self, food):
-        self.food = food
-        print(f'I have {self.food} in my fridge.')  
+        self.food.remove(food)
+        print(f'I have removed {food} from my fridge.')  
+
+    def list_of_food(self):
+        print(f"List of food is : {self.food}")
 
 myfridge = SmartFridge('Bosch', 'GSX961NSAZ')
 
@@ -32,4 +36,6 @@ myfridge.greet()
 myfridge.tell_age()
 myfridge.add_food('milk')
 myfridge.add_food('eggs')
-
+myfridge.add_food('cheese')
+myfridge.remove_food('milk')
+myfridge.list_of_food()
